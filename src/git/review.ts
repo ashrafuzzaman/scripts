a95033df95d18ef79795b302bea07919cf914ca6
AKM Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2021-04-24 22:39:08 +0600

    Added arg parsing support

--- src/git/review.ts
+++ src/git/review.ts
@@ -1,10 +1,17 @@
 import { SimpleGit } from 'simple-git';
 import { getGit } from './utils';
+import { parseArgument } from './argparser';
 
-export function run(): SimpleGit {
+function getRemoteAndBranch() {
+  const pattern = /(?<remote>\w+)\:(?<branch>\w+)?/i;
+  return parseArgument(pattern);
+}
+
+export async function run(): Promise<SimpleGit> {
+  const { remote, branch } = getRemoteAndBranch();
   const git: SimpleGit = getGit();
-  const branches = git.branchLocal();
-  console.log(branches);
+  const branchSummery = await git.branchLocal();
+  console.log('branches', branchSummery);
   return git;
 };
 
