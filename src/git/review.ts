62ef28bd7cd4428f3e351e266e2efa73191e2fb3
AKM Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2021-04-26 01:01:51 +0600

    Review script working

--- src/git/review.ts
+++ src/git/review.ts
@@ -1,5 +1,6 @@
 import { SimpleGit } from 'simple-git';
 import { withArgs, warn, info } from './utils';
+import * as readline from 'readline';
 
 const ARG_PATTERN = /(?<remote>.+)\:(?<branch>.+)?/i;
 
@@ -11,15 +12,40 @@ async function run() {
     }
 
     await addRemote(git, remote);
-    // await git.fetch(remote, branch);
+
+    info(`Fetching changes from ${remote}/${branch}`);
+    await git.fetch(remote, branch);
+
+    if (await doesBranchExists(git, branch)) {
+      const cliInterface = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+      })
+
+      cliInterface.question(`Branch exists. Do you want to reset branch(${branch}) n/Y? `, async answer => {
+        cliInterface.close();
+        if (answer === 'Y') {
+          warn('Reseting branch ...');
+          await git.checkout(branch);
+          await git.reset(['--hard', `${remote}/${branch}`]);
+        } else {
+          info('Skiping to reset branch.');
+        }
+      });
+    } else {
+      info(`Checking out branch: ${branch}`);
+      await git.checkout(['-t', `${remote}/${branch}`]);
+    }
   });
 };
 
-async function remoteExists(remotes, remoteUrl) {
-  console.log(remotes);
-  const exists = remotes.some(remote => remote.refs.fetch === remoteUrl);
-  console.log('exists: ', exists)
-  return exists;
+async function doesBranchExists(git, branch) {
+  const branches = (await git.branchLocal()).all;
+  return branches.some(_branch => _branch === branch);
+}
+
+function remoteExists(remotes, remoteUrl) {
+  return remotes.some(remote => remote.refs.fetch === remoteUrl);
 }
 
 async function addRemote(git, remote) {
@@ -29,8 +55,6 @@ async function addRemote(git, remote) {
   const { repoName } = gitRepoUrl.match(gitUrlPattern).groups;
   const remoteUrl = `git@github.com:${remote}/${repoName}.git`;
 
-  console.log('remoteUrl: ', remoteUrl);
-  
   if (!remoteExists(remotes, remoteUrl)) {
     info(`Adding remote ${remote}`);
     await git.addRemote(remote, remoteUrl);
