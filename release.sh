741d6e521ed1ae2a2d8e92a4f122a9f5d38291a0
AKM Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2022-12-12 18:06:18 +0600

    Update release process

--- release.sh
+++ release.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-cp src/git/gitconfig ~/.gitconfig
+
+[ -f ~/.gitconfig ] || cp src/git/config/gitconfig ~/.gitconfig
 touch ~/.gitignore
+
+[ -f ~/.newscred.yaml ] || cp src/bash/config/newscred.yaml ~/.newscred.yaml
 
 rm -rf out/*
 npm install
 npm run build
 npm link
 
-sudo ln -s src/bash/connect-bluetooth /usr/bin/
-sudo ln -s src/bash/disconnect-bluetooth /usr/bin/
-sudo ln -s src/bash/pull-containers /usr/bin/
-sudo ln -s src/bash/update-containers /usr/bin/
-sudo ln -s src/bash/fix-camlink /usr/bin/
+base_path=$(pwd)
+sudo rm -rf /usr/bin/connect-bluetooth && sudo ln -s "$base_path/src/bash/connect-bluetooth" /usr/bin/
+sudo rm -rf /usr/bin/disconnect-bluetooth && sudo ln -s "$base_path/src/bash/disconnect-bluetooth" /usr/bin/
+sudo rm -rf /usr/bin/pull-containers && sudo ln -s "$base_path/src/bash/pull-containers" /usr/bin/
+sudo rm -rf /usr/bin/update-containers && sudo ln -s "$base_path/src/bash/update-containers" /usr/bin/
+sudo rm -rf /usr/bin/fix-camlink && sudo ln -s "$base_path/src/bash/fix-camlink" /usr/bin/
